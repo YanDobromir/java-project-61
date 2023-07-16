99728c63a17e5c663922024524135321e4cb68f7
YanDobromir <yandobromir@gmail.com>
2023-07-16 11:50:02 +0200

    main

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -3,7 +3,6 @@ import java.util.Scanner;
 public class Cli {
     public static void getUserName() {
         Scanner scanner = new Scanner(System.in);
-        
         System.out.println("May I have your name?");
         String userName = scanner.next();
         System.out.println("Hello, " + userName + "!");
