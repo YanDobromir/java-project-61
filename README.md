118b6ffba2ceefe063d4c57e4b9d342d98d8adde
YanDobromir <yandobromir@gmail.com>
2023-07-16 11:44:16 +0200

    settings projects

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/YanDobromir/java-project-61/workflows/hexlet-check/badge.svg)](https://github.com/YanDobromir/java-project-61/actions)
+
+[![Maintainability](https://api.codeclimate.com/v1/badges/bb562556d6d2f0ef1e0c/maintainability)](https://codeclimate.com/github/YanDobromir/java-project-61/maintainability)
--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -1,2 +1,12 @@
-package hexlet.code;public class Cli {
+package hexlet.code;
+import java.util.Scanner;
+public class Cli {
+    public static void getUserName() {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("May I have your name?");
+        String userName = scanner.next();
+        System.out.println("Hello, " + userName + "!");
+        scanner.close();
+    }
 }
