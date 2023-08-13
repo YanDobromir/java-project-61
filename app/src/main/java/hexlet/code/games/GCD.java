d6096a0b45e997cabc9abf28012e39bb215bef67
YanDobromir <yandobromir@gmail.com>
2023-08-13 23:46:50 +0200

    fix gcm

--- app/src/main/java/hexlet/code/games/GCD.java
+++ app/src/main/java/hexlet/code/games/GCD.java
@@ -29,9 +29,8 @@ public class GCD {
         for (int i = 0; i < QUANTITYGAMES; i++) { // кол-во игр
             number = Util.gameRandome(MAX_RANDOM_NUMBERS); // генерируем случайное число
             numberTwo = Util.gameRandome(MAX_RANDOM_NUMBERS); // генерируем случайное число
-            numberAndcorectAnswer[i][0] = Integer.toString(number); // записываем числа
-            numberAndcorectAnswer[i][1] = Integer.toString(numberTwo); // записываем числа
-            numberAndcorectAnswer[i][NUMBERS] = Integer.toString(gameLogik(number, numberTwo));
+            numberAndcorectAnswer[i][0] = number + " " + numberTwo; // записываем числа
+            numberAndcorectAnswer[i][1] = Integer.toString(gameLogik(number, numberTwo));
 
         }
         Engine.engineWorks(GAMERULEONE, numberAndcorectAnswer); // вызываем движок
